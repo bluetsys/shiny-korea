3576d4f3fc39a9eb49c4308e71d944f40e2e2f88
WooYoung <wymoon2690@gmail.com>
2021-06-14 17:26:28 +0900

    최종

--- App/ui.R
+++ App/ui.R
@@ -1,19 +1,9 @@
 library(shiny)
 
-jscode_redirect <- "Shiny.addCustomMessageHandler('redirect', function(redirect) { window.location = redirect;});"
-
 shinyUI(fluidPage(
-    tags$head(tags$script(jscode_redirect)),
-    tags$script(src = "site.js"),
     titlePanel("샤이니코리아"),
     sidebarLayout(
         sidebarPanel(
-            actionButton("login", "로그인"),
-            actionButton("roadviewshow", "로드뷰 - 보이기"),
-            actionButton("roadviewhide", "로드뷰 - 감추기"),
-            actionButton("penTo", "위치이동"),
-            actionButton("markershow", "마커표시"),
-            
             sliderInput("bins",
                         "Number of bins:",
                         min = 1,
@@ -23,11 +13,7 @@ shinyUI(fluidPage(
 
         # Show a plot of the generated distribution
         mainPanel(
-            plotOutput("distPlot"),
-            #includeHTML("html/distplot.html"),
-            includeHTML("html/map.html"),
-            textOutput("text"),
-            verbatimTextOutput("queryText")
+            plotOutput("distPlot")
         )
     )
 ))
